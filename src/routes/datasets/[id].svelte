f5151343eb9e5932c535defa629aee519f225355
Sebastian Ferreyra Pons <ushiferreyra@gmail.com>
2020-05-22 00:38:21 -0300

    More cleanup and refactoring

--- src/routes/datasets/[id].svelte
+++ src/routes/datasets/[id].svelte
@@ -1,7 +1,4 @@
 <script context='module'>
-	import JSONTree from 'svelte-json-tree'
-
-	//import { AggregationStore as aggStore } from 'app/stores';
 	import routes from 'app/data/routes.json';
 
 	export function preload({ params: {id} }) {
@@ -15,10 +12,7 @@
 </script>
 
 <script>
-	import { onMount } from 'svelte';
-	import { writable } from 'svelte/store';
-	import * as _ from 'lamb';
-
+	import DatasetExplorer from 'app/components/DatasetExplorer.svelte';
 	import { request } from 'app/net';
 	import {
 		constructQuery
@@ -30,18 +24,13 @@
 	let query;
 	let response;
 
-	function loadInfo(datasetInfo) {
-		const schema = datasetInfo.spec.dataset.schema
-		return constructQuery(schema);
-	}
-
 	function doQuery(query) {
 		const endpoint = datasetInfo.spec.dataset.endpoint_url;
 		const url = `${endpoint}/_search`;
 		return request(fetch, 'POST', url, {query});
 	}
 
-	$: query = loadInfo(datasetInfo);
+	$: query = constructQuery(datasetInfo.spec.dataset.schema);
 	$: response = doQuery(query);
 </script>
 
@@ -49,26 +38,4 @@
 	<title>dapsboard - {id}</title>
 </svelte:head>
 
-<div>
-	<h2>Dataset Information</h2>
-	<JSONTree value={datasetInfo} />
-	<h2>Query</h2>
-	<JSONTree value={query} />
-	<h2>Aggregation Results</h2>
-	{#await response}
-		Loading...
-	{:then value}
-		<JSONTree {value} />
-	{:catch error}
-		<p style="color: red">{error.message}</p>
-	{/await}
-</div>
-
-<style>
-	div {
-		width: 100%;
-		height: 100%;
-		overflow-y: auto;
-		font-family: courier;
-	}
-</style>
+<DatasetExplorer {datasetInfo} {query} {response} />
