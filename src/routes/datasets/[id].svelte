dec0a9360e2864c49457a5f20011b9b6ace6d964
Sebastian Ferreyra Pons <ushiferreyra@gmail.com>
2020-05-21 23:13:46 -0300

    Improved data loading.

--- src/routes/datasets/[id].svelte
+++ src/routes/datasets/[id].svelte
@@ -1,37 +1,48 @@
 <script context='module'>
-	import { writable } from 'svelte/store';
 	import JSONTree from 'svelte-json-tree'
 
-	import { requestAllAggregations } from 'app/elasticsearch';
 	//import { AggregationStore as aggStore } from 'app/stores';
 	import routes from 'app/data/routes.json';
 
-	const aggStore = writable("");
-
 	export function preload({ params: {id} }) {
 		const datasetInfo = routes[id];
-		const endpoint = datasetInfo.spec.dataset.endpoint_url;
-		aggStore.set("loading...");
-		let requestData;
-		if (process.browser)
-			requestData = requestAllAggregations(this.fetch, endpoint, datasetInfo.spec.dataset.schema, aggStore);
 
 		return {
 			id,
-			spec: datasetInfo.spec,
-			query: requestData,
-			aggStore
+			datasetInfo
 		}
 	}
 </script>
 
 <script>
+	import { onMount } from 'svelte';
+	import { writable } from 'svelte/store';
 	import * as _ from 'lamb';
 
+	import { request } from 'app/net';
+	import {
+		constructQuery
+	} from 'app/elasticsearch';
+
 	export let id;
-	export let spec;
-	export let query;
-	export let aggStore;
+	export let datasetInfo;
+
+	let query;
+	let response;
+
+	function loadInfo(datasetInfo) {
+		const schema = datasetInfo.spec.dataset.schema
+		return constructQuery(schema);
+	}
+
+	function doQuery(query) {
+		const endpoint = datasetInfo.spec.dataset.endpoint_url;
+		const url = `${endpoint}/_search`;
+		return request(fetch, 'POST', url, {query});
+	}
+
+	$: query = loadInfo(datasetInfo);
+	$: response = doQuery(query);
 </script>
 
 <svelte:head>
@@ -40,11 +51,17 @@
 
 <div>
 	<h2>Dataset Information</h2>
-	<JSONTree value={spec} />
+	<JSONTree value={datasetInfo} />
 	<h2>Query</h2>
 	<JSONTree value={query} />
 	<h2>Aggregation Results</h2>
-	<JSONTree value={$aggStore} />
+	{#await response}
+		Loading...
+	{:then json}
+		<JSONTree {json} />
+	{:catch error}
+		<p style="color: red">{error.message}</p>
+	{/await}
 </div>
 
 <style>
