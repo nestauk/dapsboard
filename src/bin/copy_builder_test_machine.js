4924d57cc35570aeadbe6041a627c3c6d668172e
Sebastian Ferreyra Pons <ushiferreyra@gmail.com>
2020-08-12 20:24:56 -0300

    Moved `stringifyObj` function

--- src/bin/copy_builder_test_machine.js
+++ src/bin/copy_builder_test_machine.js
@@ -1,21 +1,8 @@
+// eslint-disable-next-line node/no-extraneous-import
+import { stringifyObj } from 'svizzle/utils/obj-string'
 import { testerConfig, testerOptions } from '../machines/builder_route/tester.config';
 // eslint-disable-next-line node/no-unpublished-import
 import clip from 'clipboardy';
 
-function stringify (obj) {
-	let placeholder = '____PLACEHOLDER____';
-	let fns = [];
-	let json = JSON.stringify(obj, function (key, value) {
-		if (typeof value === 'function') {
-			fns.push(value);
-			return placeholder;
-		}
-		return value;
-	}, 2);
-	json = json.replace(new RegExp(`"${placeholder}"`, 'ug'), function () {
-		return fns.shift();
-	});
-	return json;
-}
 
-clip.write(`Machine(${JSON.stringify(testerConfig, null, 2)}, ${stringify(testerOptions)})`);
+clip.write(`Machine(${JSON.stringify(testerConfig, null, 2)}, ${stringifyObj(testerOptions)})`);
