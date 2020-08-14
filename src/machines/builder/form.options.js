20856c170a5e11c1846adf506511beb6c6cab951
Sebastian Ferreyra Pons <ushiferreyra@gmail.com>
2020-08-14 15:50:22 -0300

    Docs for actions

--- src/machines/builder/form.options.js
+++ src/machines/builder/form.options.js
@@ -1,11 +1,32 @@
 export const selectionOptions = {
 	actions: {
+		/**
+		 * Computes lists of selectable elements according to current selection.
+		 */
 		computeLists: () => false,
+		/**
+		 * Computes the request body.
+		 */
 		computeRequest: () => false,
+		/**
+		 * Deletes any nested forms
+		 */
 		deleteNestedForms: () => false,
+		/**
+		 * Determines fields and field types according to current selection.
+		 */
 		prepareForm: () => false,
+		/**
+		 * Computes the URL for the current selection and field values
+		 */
 		setURL: () => false,
+		/**
+		 * Creates a new form for configuring a subaggregation
+		 */
 		spawnNestedForm: () => false,
+		/**
+		 * Stores query results in a cache
+		 */
 		storeInCache: () => false,
 	},
 	guards: {
