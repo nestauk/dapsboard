76081d61f1e45ac4215e60da6156350c6cc3ff4d
doogyb <samueldoogan@gmail.com>
2023-01-19 12:23:33 +0000

    Configure Fastify server (#250)
    Implement a basic Fastify server, and make a PUT and GET endpoint do
    test that MongoDB documents are working correctly.
    
    closes #248

--- be/src/server.js
+++ be/src/server.js
@@ -1,8 +1,47 @@
-import { collection } from "./db.js";
+import Fastify from 'fastify';
+
+import { collection } from './db.js';
+
+
+const fastify = Fastify({
+	logger: true
+});
+
+const { PORT } = process.env || 3000;
+
+fastify.put('/', (request, reply) => {
+	collection.insertOne(request.body)
+	reply.send('Insertion succeeded')
+})
+
+const getIdSchema = {
+	params: {
+		type: 'object',
+		properties: {
+			id: { type: 'number' }
+		}
+	}
+}
+fastify.get('/:id', { schema: getIdSchema }, async (request, reply) => {
+
+	const { id } = request.params;
+	const query = { id }
+	const result = await collection.findOne(query)
+	const replyBody = result || { error: `No doc with id ${id} found` }
+	const code = result ? 200 : 404;
+	reply.code(code).send(replyBody)
+})
 
 const cursor = await collection.find({})
-// print a message if no documents were found
-if (await cursor.count() === 0) {
-	console.log("No documents found!");
-}
-await cursor.forEach(console.dir)
+cursor.forEach(console.log)
+
+const start = async () => {
+	try {
+		await fastify.listen({ host: '0.0.0.0', port: PORT });
+	} catch (err) {
+		fastify.log.error(err);
+		throw new Error(err);
+	}
+};
+
+start()
