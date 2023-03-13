3c2eb8526b7d6c6aa05b533a5bdc855134fb5a6b
doogyb <samueldoogan@gmail.com>
2023-03-13 18:09:00 +0000

    remove logs

--- be/src/db.js
+++ be/src/db.js
@@ -7,8 +7,6 @@ const client = new MongoClient(uri);
 const database = client.db('dapsboard');
 export const cache = database.collection('cache');
 
-console.log(cache);
-
 export const cacheRequest = (request, aggregation) => {
 	const fresh = {
 		_id: request.hash,
