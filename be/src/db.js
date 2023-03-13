4adf885ce541716386e81a31ce40541d49cd9cc8
doogyb <samueldoogan@gmail.com>
2023-03-13 18:43:38 +0000

    Add password support for MongoDB container (#310)
    * Add password support for MongoDB container
    
    closes #309
    
    * remove logs

--- be/src/db.js
+++ be/src/db.js
@@ -1,7 +1,7 @@
 import { MongoClient } from 'mongodb';
 
-// Replace the uri string with your connection string.
-const uri = "mongodb://mongo";
+const { MONGO_ROOT_USER: user, MONGO_ROOT_PASSWORD: password } = process.env;
+const uri = `mongodb://${user}:${password}@mongo`;
 const client = new MongoClient(uri);
 
 const database = client.db('dapsboard');
