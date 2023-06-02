fdd859cf0c22a6bda0dea81515bb9ddf23f0a5c9
doogyb <samueldoogan@gmail.com>
2023-06-02 17:37:05 +0100

    Ingest 319_hpmt_gold_interim_v3 (#320)
    Ingest 319_hpmt_gold_interim_v3
    
    closes #319
    
    * change schema
    
    * add to protected datasets
    
    * remove `property_uprn` from mappings
    
    * Update schema
    
    ---------
    
    Co-authored-by: Luca Bonavita <mindrones@users.noreply.github.com>

--- be/src/conf.js
+++ be/src/conf.js
@@ -7,5 +7,7 @@ export const PROTECTED_DATASETS = [
 	'epc_with_hp_install_dates',
 	'processed_mcs_installations',
 	'hpmt_gold_interim_v0',
-	'hpmt_gold_interim_v1'
+	'hpmt_gold_interim_v1',
+	'hpmt_gold_interim_v2',
+	'hpmt_gold_interim_v3'
 ];
